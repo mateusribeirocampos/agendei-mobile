0fd397b0e0c614bcb7cb4e38f4f405fe77d5d977
mateusribeirocampos <mateusrcampos@gmail.com>
2024-10-24 16:05:07 -0300

    Added doctor data in abahome.jsx

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import Abahome from "./src/screens/abaHome/abahome.jsx";
 import Account from "./src/screens/account/account.jsx";
 import Login from "./src/screens/login/login.jsx";
 
@@ -5,7 +6,8 @@ function App() {
 
   return <>
   
-      <Account/>
+
+      <Abahome/>
 
     </>;
 }
---
+++ src/screens/abaHome/abahome.style.js
@@ -0,0 +1,18 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js"
+
+export const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    paddingTop: 20,
+    
+  },
+
+  text: {
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: FONT_SIZE.md,
+    color: COLORS.gray1,
+    margin: 15,
+  }
+}
